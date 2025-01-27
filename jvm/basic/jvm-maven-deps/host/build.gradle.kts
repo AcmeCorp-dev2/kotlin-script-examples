e936aad903d10481891674c6d1ee0e42a7aa0c48
prudhvinadh007 <163080374+prudhvinadh007@users.noreply.github.com>
2025-01-27 18:06:23 +0530

    Update build.gradle.kts

--- jvm/basic/jvm-maven-deps/host/build.gradle.kts
+++ jvm/basic/jvm-maven-deps/host/build.gradle.kts
@@ -8,7 +8,7 @@ val kotlinVersion: String by rootProject.extra
 dependencies {
     implementation(project(":jvm:basic:jvm-maven-deps:script"))
     implementation("org.jetbrains.kotlin:kotlin-scripting-common:${kotlinVersion}")
-    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:${kotlinVersion}")
     implementation("org.jetbrains.kotlin:kotlin-scripting-jvm-host:$kotlinVersion")
     testImplementation("junit:junit:4.13.1")
     testRuntimeOnly("org.slf4j:slf4j-nop:1.7.28")
